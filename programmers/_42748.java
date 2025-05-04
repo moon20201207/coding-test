20cfdfc103f0c54c50ca6b922cc12f071d0b6bf2
moon <moon20201207@gmail.com>
2025-05-04 18:22:26 +0900

    K번째 수

---
+++ programmers/_42748.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+// K번째 수
+class _42748 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i=0; i<commands.length ; i++) {
+            int[] sliced = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(sliced);
+
+            answer[i] = sliced[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
